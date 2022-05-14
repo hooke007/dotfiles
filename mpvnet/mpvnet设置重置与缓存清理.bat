2e7d4b0c433aa517afe761dab3706900dde02a9a
hooke007 <hooke007@qq.com>
2022-05-14 19:20:31 +0100

    update mpvnet

--- mpvnet/mpvnet设置重置与缓存清理.bat
+++ mpvnet/mpvnet设置重置与缓存清理.bat
@@ -1,4 +1,5 @@
 @echo off
+chcp 936
 
 cd /D %~dp0\portable_config
 echo ==================
@@ -6,13 +7,6 @@ del input.conf
 del mpv.conf
 del mpvnet.conf
 del settings.xml
-echo ==================
-echo 清理缓存：校色档
-del icc_cache\*.*
-echo 清理缓存：着色器
-del shaders_cache\*.*
-echo 缓存：文件状态记录
-del watch_later\*.*
 echo ==================
 echo 全部执行完毕
 pause
